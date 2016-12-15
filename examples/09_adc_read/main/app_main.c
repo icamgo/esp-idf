dbaa77be65c430e9255e94a4ae65b0044510896c
Jack Tan <jiankemeng@gmail.com>
2016-12-15 13:21:12 +0800

    examples: update the adc read

--- examples/09_adc_read/main/app_main.c
+++ examples/09_adc_read/main/app_main.c
@@ -14,58 +14,53 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "soc/rtc_io_reg.h"
+#include "soc/sens_reg.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
 
-enum adc1_pad { 
-	ADC1_GPIO36 = 0,
-	ADC1_GPIO37,
-	ADC1_GPIO38,
-	ADC1_GPIO39, 
-	ADC1_GPIO32,
-	ADC1_GPIO33,
-	ADC1_GPIO34,
-	ADC1_GPIO35 
-};
- 
-enum adc1_atten {
-	ADC1_ATTEN_0DB = 0,
-	ADC1_ATTEN_3DB,
-	ADC1_ATTEN_6DB,
-	ADC1_ATTEN_12DB
-};
-
-uint32_t adc1_read(enum adc1_pad pad, enum adc1_atten att);
-
+#include "driver/gpio.h"
+#include "driver/rtc_io.h"
+#include "driver/adc.h"
 
 /* 
  * Read the amp adc, IO36 as the ADC_PRE_AMP
  * make sure connecting a 270pF cap from
  * esp32_pin5 to esp32_pin6
  */
-uint32_t adc1_amp_read();
+//uint32_t adc1_amp_read();
 
-void read_adc1_task(void *pvParameters)
+void read_adc_task(void *pvParameters)
 {
 	while (1) {
-		//printf("ESP32 RTCIO ADC PAD REG  = 0x%08X\n", *((uint32_t *)RTC_IO_ADC_PAD_REG));
-		printf("ESP32 onchip ADC1 (IO36) = %d\n", adc1_read(ADC1_GPIO36, ADC1_ATTEN_12DB));
-		printf("ESP32 onchip ADC1 (IO39) = %d\n", adc1_read(ADC1_GPIO39, ADC1_ATTEN_12DB));
-		printf("ESP32 onchip ADC1 (IO34) = %d\n", adc1_read(ADC1_GPIO34, ADC1_ATTEN_12DB));
-		printf("ESP32 onchip ADC1 (IO35) = %d\n", adc1_read(ADC1_GPIO35, ADC1_ATTEN_12DB));
-		printf("ESP32 ADC PRE AMP (IO36) = %d\n\n", adc1_amp_read());
+		printf("----------------------------------------------\n");
+		printf("ESP32 ADC1_CH0 (IO36) = %d\n", adc1_get_voltage(ADC1_CHANNEL_0));
+		printf("ESP32 ADC1_CH1 (IO39) = %d\n", adc1_get_voltage(ADC1_CHANNEL_1));
+		printf("ESP32 ADC1_CH6 (IO34) = %d\n", adc1_get_voltage(ADC1_CHANNEL_6));
+		printf("ESP32 ADC1_CH7 (IO35) = %d\n", adc1_get_voltage(ADC1_CHANNEL_7));
+
 		vTaskDelay(4000 / portTICK_PERIOD_MS);
 	}
+}
+
+void adc1_init()
+{
+	adc1_config_width(ADC_WIDTH_12Bit);
+
+	adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_0db);
+	adc1_config_channel_atten(ADC1_CHANNEL_1,ADC_ATTEN_0db);
+	adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_0db);
+	adc1_config_channel_atten(ADC1_CHANNEL_7,ADC_ATTEN_0db);
 }
 
 void app_main()
 {
 	nvs_flash_init();
-	system_init();
+	adc1_init();
+
 	printf("Welcome to Noduino Quantum\r\n");
 	printf("Try to test the ADC1 pin of the ESP32 ... \r\n");
-	xTaskCreatePinnedToCore(&read_adc1_task, "read_adc1_task", 1024, NULL, 5,
+	xTaskCreatePinnedToCore(&read_adc_task, "read_adc_task", 2048, NULL, 5,
 				NULL, 0);
 }
